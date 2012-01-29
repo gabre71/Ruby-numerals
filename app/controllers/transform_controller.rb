ee76e53ba38241e52b03cfbdf035dea05d517305
rene <gabre71@gmail.com>
2012-01-29 16:56:12 +0100

    Controller added

---
+++ app/controllers/transform_controller.rb
@@ -0,0 +1,5 @@
+class TransformController < ApplicationController
+  def new
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyNumerals::Application.config.secret_token = 'b41f2816c81304415415eacfbb1e937e4273e03ebff3031c09be4e2de50215739b91a36dbfed491c30ff25d6ec15c44a8291b7de7bf18c1c63550062c76c8434'
