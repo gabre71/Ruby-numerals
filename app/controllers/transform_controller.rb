048a9df1956a161fa07bd8f0c9b17883c90ed92e
rene <gabre71@gmail.com>
2012-01-29 18:08:10 +0100

    Transform class added

--- app/controllers/transform_controller.rb
+++ app/controllers/transform_controller.rb
@@ -1,9 +1,11 @@
 class TransformController < ApplicationController
   def new
-
   end
 
   def create
+    @transform = Transform.new
+    @transform.number = params[:number]
+    @transform.text_value
     respond_to do |format|
       format.html { render :action => "new" }
     end
--- app/models/transform.rb
+++ app/models/transform.rb
@@ -1,3 +1,10 @@
+require 'linguistics'
+Linguistics::use( :en )
+
 class Transform
-  
+  attr_accessor :number, :result
+
+  def text_value
+    self.result = number.to_i.en.numwords
+ 	end
 end
